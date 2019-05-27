918ffbd6237bf427f6a95371d5e2a4776b0030db
Thuchpun Apivitcholachat <41178310+AnTznimalz@users.noreply.github.com>
2019-05-27 20:07:48 +0700

    Update buzzer.ino

--- buzzer.ino
+++ buzzer.ino
@@ -1,11 +1,11 @@
 void setup() {
-  pinMode(5, OUTPUT); // ตั้งค่าให้ขาD0 เป็น output
+  pinMode(D0, OUTPUT); // ตั้งค่าให้ขาD0 เป็น output
 
 }
 
 void loop() {
-  digitalWrite(5, HIGH); // ปล่อยไฟเข้าขาD0 ให้buzzer ดัง
+  digitalWrite(D0, HIGH); // ปล่อยไฟเข้าขาD0 ให้buzzer ดัง
   delay(1000); //delay 1 วิ
-  digitalWrite(5, LOW); // ปล่อยไฟออกจากขา D0 
+  digitalWrite(D0, LOW); // ปล่อยไฟออกจากขา D0 
   delay(1000); //delay 1 วิ
 }
